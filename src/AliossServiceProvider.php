4d69c5ef8d1eee6588490a760e61dbaf2a7b897f
siaoynli <siaoynli@126.com>
2019-11-19 20:06:05 +0800

    ok

--- src/AliossServiceProvider.php
+++ src/AliossServiceProvider.php
@@ -23,7 +23,7 @@ class AliossServiceProvider extends ServiceProvider
     {
 
         //自定义文件系统 extend 方法的第一个参数是驱动程序的名称，第二个参数是接收 $app 及 $config 变量的闭包。该解析闭包必须返回 League\Flysystem\Filesystem 的实例。$config 变量包含了特定磁盘在 config/filesystems.php 中定义的值。
-        Storage::extend('alioss', function ($app, $config)  {
+        Storage::extend('alioss', function ($app, $config) {
             return new Filesystem(new AliossAdapter($this->ossClient, $config['bucket']));
         });
     }
@@ -36,37 +36,34 @@ class AliossServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(
-            __DIR__.'/../config/alioss.php','alioss'
+            __DIR__ . '/../config/alioss.php',
+            'alioss'
         );
 
-        $config=$this->app['config'];
+        $config = $this->app['config'];
 
-        $arr=[
-            "alioss"=>array_merge(['driver' => 'alioss'],config("alioss"))
+        $arr = [
+            "alioss" => array_merge(['driver' => 'alioss'], config("alioss"))
         ];
 
-        $config->set("filesystems.disks",array_merge($config->get("filesystems.disks"),$arr));
+        $config->set("filesystems.disks", array_merge($config->get("filesystems.disks"), $arr));
 
         try {
-            $ossConfig=config("alioss");
+            $ossConfig = config("alioss");
 
             $bucket = $ossConfig['bucket'];
             // 创建OSS客户端
             $this->ossClient = new OssClient($ossConfig['key'], $ossConfig['secret'], $ossConfig['region']);
 
-            if(!$this->ossClient) {
-                throw new \Exception("OssClient Error");
+            if (!$this->ossClient) {
+                throw new OssException("Oss Client is Null");
             }
             // 单例绑定服务
             $this->app->singleton('alioss', function ($app) use ($bucket) {
                 return new Alioss($this->ossClient, $bucket);
             });
-
         } catch (OssException $e) {
             logger('Alioss service init error: ' . $e->getMessage());
-            throw new \Exception($e->getMessage());
         }
     }
-
-
 }
